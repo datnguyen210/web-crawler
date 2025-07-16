0f7b154a8dc4a537c56d87905fe1bfa518e14842
datnguyen210 <datnguyenftu210@gmail.com>
2025-07-17 06:54:55 +0700

    feat: extract title and all links from the a page

--- main.go
+++ main.go
@@ -1,13 +1,57 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
+
+	"golang.org/x/net/html"
 )
 
+func getHref(token html.Token) (bool, string) {
+	for _, a := range token.Attr {
+		if a.Key == "href" {
+			if len(a.Val) == 0 || !strings.HasPrefix(a.Val, "http") {
+				return false, ""
+			}
+			return true, a.Val
+		}
+	}
+	return false, ""
+}
+
+func parseHTML (content []byte) (title string, links []string) {
+	tokenizer := html.NewTokenizer(bytes.NewReader(content))
+
+	for {
+		tokenType := tokenizer.Next()
+		if tokenType == html.ErrorToken {
+			break
+		}
+
+		token := tokenizer.Token()
+
+		if(tokenType == html.StartTagToken) {
+			if(token.Data == "title") {
+				tokenizer.Next()
+				titleToken := tokenizer.Token()
+				title = titleToken.Data
+			}
+
+			if token.Data == "a" {
+				if ok, href := getHref(token); ok {
+					links = append(links, href)
+				}
+			}
+		}
+	}	
+	return title, links
+}
+
 func main() {
-	url := "https://go.dev/learn/"
+	url := "https://pkg.go.dev/golang.org/x/net/html"
 
 	res, err := http.Get(url)
 	if(err != nil) {
@@ -23,13 +67,8 @@ func main() {
 		return
 	}
 
-	fmt.Printf("Successfully fetching %s\n", url)
-	fmt.Printf("Response status: %s\n", res.Status)
-	fmt.Printf("Response body length: %d\n", len(body))
+	title, links := parseHTML(body)
 
-	if(len(body) > 200) {
-		fmt.Printf("First 200 characters of the body: %s\n", string(body[:200]))
-	} else {
-		fmt.Printf("Response body: %s\n", string(body))
-	}
+	fmt.Printf("Title: %s\n", title)
+	fmt.Printf("Found %d links\n", len(links))
 }
